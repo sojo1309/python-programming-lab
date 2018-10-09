8c3f83a639ab00812a11750e10f243550b72d8b2
Soham Joshi <43982964+sojo1309@users.noreply.github.com>
2018-10-09 14:05:20 +0530

    Update lab_funct.py

--- lab_funct.py
+++ lab_funct.py
@@ -1,12 +1,12 @@
-def armn(x):
-	sum1=0
+def armn(x):						#Defines the function
+	sum1=0						#assings sum1 the value 0
 	a=x
-	while (a>0):
-		l=a%10
-		sum1 += l**3
-		a=a//10
-	if sum1==x:
-		return "armstrong no"
+	while (a>0):					#starts while loop
+		l=a%10					#extracts last digit
+		sum1 += l**3				#cube of extracted digit
+		a=a//10					#gives quotient of input
+	if sum1==x:					#checks if sum equals the input 
+		return "armstrong no"			#if sum1 equals input exiots the func and prints output
 	else:
 		return 'NOT armstrong no'
 x=int(input())
