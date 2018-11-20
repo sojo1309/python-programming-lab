d827dd11f157186cd375f7156f126435c9911629
Soham Joshi <43982964+sojo1309@users.noreply.github.com>
2018-11-20 12:23:48 +0530

    Grade of student

---
+++ assignment.py
@@ -0,0 +1,12 @@
+m=int(input("enter the marks of student: "))
+if(m>=90):
+ 	print("the student has A grade")
+if(89<=m<=75):
+	print("the student has B grade")
+if(74<=m<=50):
+	print("the student has C grade")
+if(50<=m<=35):
+	print("the student has D grade")
+if(m<35):
+	print("the student has F grade")
+
