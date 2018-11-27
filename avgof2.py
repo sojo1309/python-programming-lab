010d438af8894c97178cc310909d311045162453
Soham Joshi <43982964+sojo1309@users.noreply.github.com>
2018-11-27 12:54:28 +0530

    Find the average of 2 given nos.

---
+++ avgof2.py
@@ -0,0 +1,5 @@
+
+t1=eval(input("Enter number 1 :"))    #allows to enter the first number
+t2=eval(input("Enter number 2 :"))         #allows to enter second number
+print("Average of two numbers is: ")      #prints the initial full line
+print((t1+t2)/2) #prints the final output                           
