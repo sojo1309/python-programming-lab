2c1e15a40bc9da85189d1ff1a6bbdd88ddf3aa5b
Soham Joshi <43982964+sojo1309@users.noreply.github.com>
2018-11-27 12:52:36 +0530

    Convert the given temp into some Celcius/Farherneit

--- celsius.py
+++ celsius.py
@@ -1,5 +1,5 @@
-j=int(input("enter temp"))
-k=int(input("if temp enteted is in celcius enter 1 or else press 2"))
+j=int(input("enter temp"))                                              #Takes the user input for temp
+k=int(input("if temp enteted is in celcius enter 1 or else press 2"))   #Takes the user input of whether the temp is in C or F       
 if k==1:
  a=(9*j +160)/5
  print("temp in farenheit is",a)
